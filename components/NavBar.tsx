73945ebb338ce20ab4bcead907044536bf320ead
vanha777 <vanha777@icloud.com>
2025-02-11 18:10:45 +1100

    x

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -56,7 +56,7 @@ export default function NavBar() {
                             className="bg-gradient-to-r from-[#0CC0DF] to-[#14F195] text-white px-6 py-2 rounded-lg font-medium hover:opacity-90 transition-opacity"
                         >
                             {/* Let&apos;s Build */}
-                            Sign Up
+                            Beta Access
                         </Link>
                     </div>
                 </div>
