ada5e2246d31ed22545c62d698036271ed524516
vanha777 <vanha777@icloud.com>
2025-02-12 13:26:56 +1100

    x

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,37 +43,37 @@ export default function RootLayout({
     <html lang="en" data-theme="metaloot">
       <Head>
         {/* General Meta Tags */}
-        <meta name="title" content="MetaLoot - Blockchain API For Gaming" />
+        <meta name="title" content="MetaLoot - Gaming Digital Asset Register" />
         <meta name="description" content="Ship faster, cheaper and better with our API." />
 
         {/* Open Graph / Facebook */}
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://www.metaloot.dev/" />
-        <meta property="og:title" content="MetaLoot - Blockchain API For Gaming" />
+        <meta property="og:title" content="MetaLoot - Gaming Digital Asset Register" />
         <meta property="og:description" content="Ship faster, cheaper and better with our API." />
-        <meta property="og:image" content="https://tzqzzuafkobkhygtccse.supabase.co/storage/v1/object/public/biz_touch/banner.png" />
+        <meta property="og:image" content="https://tzqzzuafkobkhygtccse.supabase.co/storage/v1/object/public/metaloot/webmetadata/metaloot.png" />
         <meta property="og:image:alt" content="A stunning preview of MetaLoot's multiverse gaming platform" />
 
         {/* Twitter */}
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:url" content="https://www.metaloot.dev/" />
-        <meta name="twitter:title" content="MetaLoot - Blockchain API For Gaming" />
+        <meta name="twitter:title" content="MetaLoot - Gaming Digital Asset Register" />
         <meta name="twitter:description" content="Ship faster, cheaper and better with our API." />
-        <meta name="twitter:image" content="https://tzqzzuafkobkhygtccse.supabase.co/storage/v1/object/public/biz_touch/banner.png" />
+        <meta name="twitter:image" content="https://tzqzzuafkobkhygtccse.supabase.co/storage/v1/object/public/metaloot/webmetadata/metaloot.png" />
         <meta name="twitter:image:alt" content="A stunning preview of MetaLoot's multiverse gaming platform" />
         <meta name="twitter:site" content="@playmetaloot" />
         <meta name="twitter:creator" content="@playmetaloot" />
 
         {/* Telegram */}
-        <meta property="og:title" content="MetaLoot - Blockchain API For Gaming" />
+        <meta property="og:title" content="MetaLoot - Gaming Digital Asset Register" />
         <meta property="og:description" content="Ship faster, cheaper and better with our API." />
-        <meta property="og:image" content="https://tzqzzuafkobkhygtccse.supabase.co/storage/v1/object/public/biz_touch/banner.png" />
+        <meta property="og:image" content="https://tzqzzuafkobkhygtccse.supabase.co/storage/v1/object/public/metaloot/webmetadata/metaloot.png" />
         <meta property="og:url" content="https://www.metaloot.dev/" />
 
         {/* Discord */}
-        <meta property="og:title" content="MetaLoot - Blockchain API For Gaming" />
+        <meta property="og:title" content="MetaLoot - Gaming Digital Asset Register" />
         <meta property="og:description" content="Ship faster, cheaper and better with our API." />
-        <meta property="og:image" content="https://tzqzzuafkobkhygtccse.supabase.co/storage/v1/object/public/biz_touch/banner.png" />
+        <meta property="og:image" content="https://tzqzzuafkobkhygtccse.supabase.co/storage/v1/object/public/metaloot/webmetadata/metaloot.png" />
         <meta property="og:type" content="website" />
       </Head>
       <body suppressHydrationWarning={true} className={inter.className}>
